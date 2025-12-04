344c3b86486c64f439de00dc295e7226d1eb9492
HungDang <20520194@gm.uit.edu.vn>
2025-12-05 01:46:17 +0700

    config cors

--- api-service/app/main.py
+++ api-service/app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routers import words, modules
 
 app = FastAPI(
     title="Server API Service - Vocabulary Backend",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],     # sửa lại sau: app mobile domain, dev domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Routers
